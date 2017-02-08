bf082d2a831f926b4496eb945b5c80d729d05756
Gauthier Levha <levha.g@gmail.com>
2017-02-08 12:01:02 +0100

    models done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,10 @@
+class Restaurant < ApplicationRecord
+
 CATEGORIES = %w(chinese italian japanese french belgian)
 
-class Restaurant < ApplicationRecord
- has_many :reviews, dependent: destroy
+ has_many :reviews, dependent: :destroy
  validates :name, presence: true
- validates :address, presence: true, uniqueness: true
- validates :category, inclusion: { in: CATEGORIES, message: "%{category} is not valid", allow_nil: false }
+ validates :address, presence: true
+ validates :category, presence: true
+ validates :category, inclusion: { in: CATEGORIES, message: "category is not valid", allow_nil: false }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
  belongs_to :restaurant
- validates :rating, inclusion: { in: %w(0..5), message: "%{rating} must be between 0 and 5" }
+ validates :content, presence: true
+ validates :rating, inclusion: { in: (0..5).to_a, message: "rating must be between 0 and 5" }
+ validates :rating, numericality: { only_integer: true }
 end
